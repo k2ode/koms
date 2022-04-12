1dd0dcd0e241b6ba6a3846f9b42c2ed9302805f7
Max Koon <max@koon.dev>
2022-04-12 14:18:42 -0400

    feat: add abook contacts

---
+++ util.go
@@ -0,0 +1,11 @@
+package koms
+
+import "strings"
+
+func Lines(str string) []string {
+	return strings.Split(str, "\n")
+}
+
+func Between(str string, start string, end string) string {
+	return strings.Split(strings.Split(str, start)[1], end)[0]
+}
---
+++ util_test.go
@@ -0,0 +1,21 @@
+package koms
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUtilLines(t *testing.T) {
+	str := "foo\nbar"
+	lines := Lines(str)
+	assert.Equal(t, lines[0], "foo")
+	assert.Equal(t, lines[1], "bar")
+}
+
+func TestUtilBetween(t *testing.T) {
+	between := Between("what is a foo bar", "what is a ", " bar")
+
+	assert.Equal(t, between, "foo")
+
+}
